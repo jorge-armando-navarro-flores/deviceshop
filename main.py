bdbbc440ae1153ea9786974d4614ac7b47da6ae9
jorge-armando-navarro-flores <jorge.armando.navarro.flores@gmail.com>
2022-04-15 22:22:01 -0500

    admin

--- main.py
+++ main.py
@@ -48,6 +48,11 @@ def portfolio_item():
     return render_template("portfolio-item.html")
 
 
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
 
