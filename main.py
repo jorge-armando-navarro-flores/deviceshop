d666be2ae32a4d693308879a70d3f75c14fd8f1c
jorge-armando-navarro-flores <jorge.armando.navarro.flores@gmail.com>
2022-05-16 17:24:13 -0500

    edit and delete

--- main.py
+++ main.py
@@ -129,6 +129,36 @@ def add_new_post():
     return render_template("make-post.html", form=form)
 
 
+@app.route("/edit-post/<int:post_id>", methods=["POST", "GET"])
+@admin_only
+def edit_post(post_id):
+    post = BlogPost.query.get(post_id)
+    edit_form = CreatePostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        body=post.body
+    )
+    if edit_form.validate_on_submit():
+        post.title = edit_form.title.data
+        post.subtitle = edit_form.subtitle.data
+        post.img_url = edit_form.img_url.data
+        post.body = edit_form.body.data
+        db.session.commit()
+        return redirect(url_for("blog_post", post_id=post.id))
+
+    return render_template("make-post.html", form=edit_form)
+
+
+@app.route("/delete/<int:post_id>")
+@admin_only
+def delete_post(post_id):
+    post_to_delete = BlogPost.query.get(post_id)
+    db.session.delete(post_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
 @app.route("/pricing")
 def pricing():
     return render_template("pricing.html")
